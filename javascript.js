4b019dadcb41b81d3891c37d53f247bb1d5fa683
Aram_Nayebbandi <devaramnye@gmail.com>
2023-10-06 17:54:03 +0200

    finish project

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 const email = document.getElementById("mail");
 const form = document.getElementById("form");
+const button = document.querySelector(".buttontwo");
 
 email.addEventListener("input", (event) => {
     if (!email.validity.valid) {
@@ -12,5 +13,24 @@ email.addEventListener("input", (event) => {
 form.addEventListener("submit", (event) => {
     if (!email.validity.valid) {
         event.preventDefault();
+    } else if (email.validity.valid) {
+        event.preventDefault();
+        document.querySelector("#header").innerHTML = "";
+        document.querySelector("#header").innerHTML = "<img class='success' src='./assets/images/icon-success.svg' />";
+        document.querySelector(".main__h2").textContent = "Thanks for subscribing!";
+        document.querySelector("ul").classList.add("hidden");
+        document.querySelector(".buttonone").classList.add("hidden");
+        document.querySelector(".buttontwo").classList.remove("hidden");
+        document.getElementById("canvas").style['flex-direction'] = "column";
+        document.getElementById("canvas").style['align-items'] = "flex-start";
+        document.getElementById("canvas").style['max-width'] = "400px";
+        document.querySelector("form").classList.add("hidden");
+        document.querySelector(".main__p").classList.add("hidden");
+        document.querySelector(".main__pp").classList.remove("hidden")
+        document.querySelector(".email-mess").textContent = email.value;
     }
 });
+
+button.addEventListener("click", (event) => {
+    location.reload();
+})
